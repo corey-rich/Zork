34a1a14fa95db11a164a19d56238041ac8d9535a
corey-rich <43017362+corey-rich@users.noreply.github.com>
2019-10-03 21:56:57 -0400

    A Larger World

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,113 +8,21 @@ namespace Zork
 {
     class Program
     {
-        private static Room CurrentRoom
-        {
-            get
-            {
-                return Rooms[Location.Row, Location.Column];
-            }
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Zork!");
-
-            const string defaultRoomsFilename = "Rooms.Json";
+            const string defaultRoomsFilename = "Zork.json";
             string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
-            InitializeRooms(roomsFilename);
 
-            Room previousRoom = null;
-            Commands command = Commands.UNKNOWN;
-            while (command != Commands.QUIT)
-            {
-                Console.WriteLine(CurrentRoom);
-
-                if (previousRoom != CurrentRoom)
-                {
-                    Console.WriteLine(CurrentRoom.Description);
-                    previousRoom = CurrentRoom;
-                }
-
-                Console.Write("> ");
-                command = ToCommand(Console.ReadLine().Trim());
-
-                switch (command)
-                {
-                    case Commands.QUIT:
-                        Console.WriteLine("Thank you for playing!");
-                        break;
-
-                    case Commands.LOOK:
-                        Console.WriteLine(CurrentRoom.Description);
-                        break;
-
-                    case Commands.NORTH:
-                    case Commands.SOUTH:
-                    case Commands.EAST:
-                    case Commands.WEST:
-                        if (Move(command) == false)
-                        {
-                            Console.WriteLine("The way is shut!");
-                        }
-                        break;
-
-                    default:
-                        Console.WriteLine("Unknown command.");
-                        break;
-                }
-            }
+            Game game = Game.Load(roomsFilename);
+            Console.WriteLine("Welcome to Zork!");
+            game.Run();
+            Console.WriteLine("Thank you for playing!");
         }
-        private static bool Move(Commands command)
-        {
-            Assert.IsTrue(Directions.Contains(command), "Invalid direction.");
-
-            bool movedSuccessfully = true;
-            switch (command)
-            {
-                case Commands.SOUTH when Location.Row > 0:
-                    Location.Row--;
-                    break;
-
-                case Commands.NORTH when Location.Row < Rooms.GetLength(0) - 1:
-                    Location.Row++;
-                    break;
-
-                case Commands.EAST when Location.Column < Rooms.GetLength(1) - 1:
-                    Location.Column++;
-                    break;
-
-                case Commands.WEST when Location.Column > 0:
-                    Location.Column--;
-                    break;
-
-                default:
-                    movedSuccessfully = false;
-                    break;
-            }
-
-            return movedSuccessfully;
-        }
-
-        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-
-        private static void InitializeRooms(string roomsFilename) =>
-            Rooms = JsonConvert.DeserializeObject<Room[,]>(File.ReadAllText(roomsFilename));
-
-        private static Room[,] Rooms;                           
-
-        private static readonly List<Commands> Directions = new List<Commands>
-        {
-            Commands.NORTH,
-            Commands.SOUTH,
-            Commands.EAST,
-            Commands.WEST
-        };
 
         private enum CommandLineArguments
         {
             RoomsFilename = 0
         }
-
-        private static (int Row, int Column) Location = (1, 1);
     }
 }
+
